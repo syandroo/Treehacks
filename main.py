018ae05aeb652c718d39f370742368a09669f6f4
andrewsy97 <andrewsy97@gmail.com>
2016-02-13 02:52:10 -0800

    Sentence splitter and basic todo list

---
+++ main.py
@@ -0,0 +1,60 @@
+from alchemyapi import AlchemyAPI
+import json
+
+demo_text = "Yesterday dumb Bob destroyed my fancy iPhone in beautiful Denver, Colorado. I guess I will have to head over to the Apple Store and buy a new one."
+
+abbreviations = {'dr.': 'doctor', 'mr.': 'mister', 'bro.': 'brother', 'bro': 'brother', 'mrs.': 'mistress', 'ms.': 'miss', 'jr.': 'junior', 'sr.': 'senior',
+                 'i.e.': 'for example', 'e.g.': 'for example', 'vs.': 'versus'}
+terminators = ['.', '!', '?']
+wrappers = ['"', "'", ')', ']', '}']
+
+# TODO - extract text from article
+
+# splits sentences from paragraphs ====================================
+def find_sentences(paragraph):
+   end = True
+   sentences = []
+   while end > -1:
+       end = find_sentence_end(paragraph)
+       if end > -1:
+           sentences.append(paragraph[end:].strip())
+           paragraph = paragraph[:end]
+   sentences.append(paragraph)
+   sentences.reverse()
+   return sentences
+
+
+def find_sentence_end(paragraph):
+    [possible_endings, contraction_locations] = [[], []]
+    contractions = abbreviations.keys()
+    sentence_terminators = terminators + [terminator + wrapper for wrapper in wrappers for terminator in terminators]
+    for sentence_terminator in sentence_terminators:
+        t_indices = list(find_all(paragraph, sentence_terminator))
+        possible_endings.extend(([] if not len(t_indices) else [[i, len(sentence_terminator)] for i in t_indices]))
+    for contraction in contractions:
+        c_indices = list(find_all(paragraph, contraction))
+        contraction_locations.extend(([] if not len(c_indices) else [i + len(contraction) for i in c_indices]))
+    possible_endings = [pe for pe in possible_endings if pe[0] + pe[1] not in contraction_locations]
+    if len(paragraph) in [pe[0] + pe[1] for pe in possible_endings]:
+        max_end_start = max([pe[0] for pe in possible_endings])
+        possible_endings = [pe for pe in possible_endings if pe[0] != max_end_start]
+    possible_endings = [pe[0] + pe[1] for pe in possible_endings if sum(pe) > len(paragraph) or (sum(pe) < len(paragraph) and paragraph[sum(pe)] == ' ')]
+    end = (-1 if not len(possible_endings) else max(possible_endings))
+    return end
+
+
+def find_all(a_str, sub):
+    start = 0
+    while True:
+        start = a_str.find(sub, start)
+        if start == -1:
+            return
+        yield start
+        start += len(sub)
+# end sentences ===============================================
+
+# TODO extract group messages, should be clean using groupme api call
+
+# TODO return sentiments of each sentence in array
+
+# TODO show when changes in sentiment happens - probably important in convo
