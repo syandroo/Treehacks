ed371f41ae52f374982bfd92b7372d73d4f94dc2
kerhardt <kerhardt@mit.edu>
2016-02-14 02:44:36 -0800

    really the original

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -1,13 +1,10 @@
 """
 Routes and views for the flask application.
 """
-import os
+
 from datetime import datetime
-from flask import render_template, request
+from flask import render_template
 from FlaskWebProject import app
-
-from generate_summary_json import generate_summary_json
-
 
 @app.route('/')
 @app.route('/home')
@@ -18,18 +15,3 @@ def home():
         title='Home Page',
         year=datetime.now().year,
     )
-
-
-# @app.route('/summarize', methods=['GET'])
-# def summarize():
-#     access_token = os.getenv('TEST_TEAM_SLACK_ACCESS_TOKEN')
-#     member_id = request.args.get('user_id')
-#     channel_id = request.args.get('channel_id')
-#     channel_name = request.args.get('channel_name')
-#     num_messages = request.args.get('text')
-#     summary_json = generate_summary_json(member_id, channel_id, channel_name, num_messages, access_token)
-#     return {'text': channel_name, 'private': True}
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
