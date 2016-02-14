8f941462aa06f4b286cbc07261524d8695e06e76
Tianlin Shi <tianlins@stanford.edu>
2016-02-13 23:25:00 -0800

    add visualization of summarization

--- server/server.py
+++ server/server.py
@@ -3,7 +3,9 @@ from tornado import (ioloop, web)
 import simplejson as json
 
 from .config import SERVER_ROOT
-from segmentation.cluster import spectral_clustering
+from segmentation.cluster import spectral_clustering, split_messages
+
+import text_analysis
 #messages = [
 #    '''
 #    Don't forget it's Valentine's Day tomorrow!!
@@ -27,16 +29,51 @@ except Exception as e:
     print '[server] error: ', e.message
 
 
+def segmentation():
+    messages_data = get_slack_messages(15)
+    channel_name = messages_data['channel_name']
+    messages = messages_data['messages']
+    labels = spectral_clustering(messages, num_clusters=5)
+    return (channel_name, messages, labels)
+
+
+def summarization(messages, labels):
+    messages_split = split_messages(messages, labels)
+    messages_stacked = [' '.join(
+                                [message['text'] for message in messages]
+                            ) for messages in messages_split
+                        ]
+    summaries = []
+    for message in messages_stacked:
+        print message
+        summary = text_analysis.groupSummary(message)
+        summaries.append(summary)
+    return summaries
+
+
 class SegmentationHandler(web.RequestHandler):
+    '''
+    visualize segmentation results.
+    '''
     def get(self):
-        messages_data = get_slack_messages(15)
-        channel_name = messages_data['channel_name']
-        messages = messages_data['messages']
-        labels = spectral_clustering(messages, num_clusters=5)
+        (channel_name, messages, labels) = segmentation()
         self.render('segmentation.html', data=json.dumps({
                     'messages': messages,
                     'labels': list([int(label) for label in labels]),
                 }), channel_name=channel_name)
+
+class SummarizationHandler(web.RequestHandler):
+    '''
+    visualize summerization results
+    '''
+    def get(self):
+        (channel_name, messages, labels) = segmentation()
+        summaries = summarization(messages, labels)
+        self.render('summarization.html',
+                summaries=json.dumps(summaries),
+                labelset = json.dumps(list([int(label) for label in set(labels)])),
+                channel_name=channel_name)
+
 
 
 handlers = [
@@ -46,6 +83,7 @@ handlers = [
     (r"/(.*\.js)", web.StaticFileHandler, {"path": SERVER_ROOT + "/frontend/js/"}),
     # main routes.
     (r"/segmentation", SegmentationHandler),
+    (r"/summarization", SummarizationHandler),
 ]
 
 settings = {
