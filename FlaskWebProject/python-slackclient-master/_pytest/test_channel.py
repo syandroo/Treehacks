dff3fb4c589d4d5f2e0128428c290622fd20205d
kerhardt <kerhardt@mit.edu>
2016-02-14 03:34:24 -0800

    add SlackClient to requirements

--- FlaskWebProject/python-slackclient-master/_pytest/test_channel.py
+++
@@ -1,9 +0,0 @@
-from slackclient._channel import Channel
-import pytest
-
-def test_Channel(channel):
-    assert type(channel) == Channel
-
-@pytest.mark.xfail
-def test_Channel_send_message(channel):
-    channel.send_message('hi')
