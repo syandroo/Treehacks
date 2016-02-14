91464ac69775a1b0cd00ad4564955a4a4d42bc13
kerhardt <kerhardt@mit.edu>
2016-02-14 00:27:34 -0800

    removed tokens

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -2,9 +2,14 @@
 Routes and views for the flask application.
 """
 
-from datetime import datetime
-from flask import render_template
+from flask import render_template, request
 from FlaskWebProject import app
+
+from oauth_constants import TEST_TEAM_SLACK_ACCESS_TOKEN 
+from generate_summary_json import generate_summary_json
+
+global TEST_TEAM_SLACK_ACCESS_TOKEN
+
 
 @app.route('/')
 @app.route('/home')
@@ -12,6 +17,20 @@ def home():
     """Renders the home page."""
     return render_template(
         'index.html',
-        title='Home Page',
-        year=datetime.now().year,
+        title='Home Page'
     )
+
+
+# text is number of messages
+@app.route('/summarize', methods=['GET'])
+def summarize():
+    member_id = requests.args.get('user_id')
+    channel_id = requests.args.get('channel_id')
+    channel_name = requests.args.get('channel_name')
+    num_messages = requests.args.get('text')
+    summary_json = generate_summary_json(member_id, channel_id, channel_name, num_messages, TEST_TEAM_SLACK_ACCESS_TOKEN)
+    return {'text': channel_name, 'private': True}
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ slackbot_summary_data.py
@@ -0,0 +1,10 @@
+'token'=nIksjkW3qckhThROc05XJPYC
+'team_id'=T0001
+'team_domain'=example
+'channel_id'=C2147483705
+'channel_name'=test
+'user_id'=U2147483697
+'user_name'=Steve
+'command'=/weather
+'text'=94070
+'response_url'=https://hooks.slack.com/commands/1234/5678
