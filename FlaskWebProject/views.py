c10a4b6f68fb5e04d512d39f2ab50b10e527411a
kerhardt <kerhardt@mit.edu>
2016-02-14 02:29:26 -0800

    reset views

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -1,16 +1,10 @@
 """
-Routes and views for the Flask application.
+Routes and views for the flask application.
 """
-import os
 
-from flask import render_template, request
+from datetime import datetime
+from flask import render_template
 from FlaskWebProject import app
-
-from generate_summary_json import generate_summary_json
-
-
-ACCESS_TOKEN = os.getenv('TEST_TEAM_SLACK_ACCESS_TOKEN')
-
 
 @app.route('/')
 @app.route('/home')
@@ -18,19 +12,6 @@ def home():
     """Renders the home page."""
     return render_template(
         'index.html',
-        title='Home Page'
+        title='Home Page',
+        year=datetime.now().year,
     )
-
-
-@app.route('/summarize', methods=['GET'])
-def summarize(ACCESS_TOKEN):
-    member_id = requests.args.get('user_id')
-    channel_id = requests.args.get('channel_id')
-    channel_name = requests.args.get('channel_name')
-    num_messages = requests.args.get('text')
-    summary_json = generate_summary_json(member_id, channel_id, channel_name, num_messages, ACCESS_TOKEN)
-    return {'text': channel_name, 'private': True}
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
